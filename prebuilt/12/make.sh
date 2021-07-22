847b3b70debf939eeb3d06359e835b779934fe0a
Nippon <37734266+nnippon@users.noreply.github.com>
2021-07-22 18:20:47 +0300

    prebuilt: enable ro.apex.updatable
    idk but seems now needed

--- prebuilt/12/make.sh
+++ prebuilt/12/make.sh
@@ -7,6 +7,7 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 # Deal with non-flattened apex
 $thispath/../../scripts/apex12_extractor.sh $1/apex
 $thispath/../../scripts/apex12_extractor.sh $1/system_ext/apex
+echo "ro.apex.updatable=true" >> $1/product/build.prop
 
 # Copy system files
 rsync -ra $thispath/system/ $systempath
