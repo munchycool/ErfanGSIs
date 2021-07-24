d46a0b76e9c629ffb1a3a3c077604728d0df861c
Nippon <37734266+nnippon@users.noreply.github.com>
2021-07-24 22:58:01 +0300

    roms: 12: another try for decryption

--- roms/12/Pixel/system/bin/fix_ex_sdcard.sh
+++ roms/12/Pixel/system/bin/fix_ex_sdcard.sh
@@ -1,3 +1,10 @@
 #!/system/bin/sh
-sleep 90
-rm -rf /data/system/storage.xml
+while true
+do
+
+rm -f /data/system/storage.xml
+touch /data/system/storage.xml
+chattr +i /data/system/storage.xml
+sleep 1
+
+done
