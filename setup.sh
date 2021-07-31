2d26ffc022f8610fb6eea4384844526e3f2f617d
Nippon <37734266+nnippon@users.noreply.github.com>
2021-07-31 16:48:22 +0300

    ErfanGSI: setup.sh: use pycryptodome for python3
    as pycrypto and crypto is ded on Python3 keks

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 
 sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev brotli liblz4-tool axel gawk aria2 detox cpio rename build-essential simg2img aria2 python3-pip default-jre -y
 pip3 install wheel setuptools
-pip3 install backports.lzma docopt zstandard bsdiff4 protobuf pycrypto
+pip3 install backports.lzma docopt zstandard bsdiff4 protobuf pycryptodome
 sudo pip3 install --upgrade protobuf
